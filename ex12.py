e21f9636d71300706cff24b04b06e7328086240d
Osman Rahim Khan <osmanrkhan1@gmail.com>
2016-10-31 23:16:10 +0500

    Update ex12.py

--- ex12.py
+++ ex12.py
@@ -7,10 +7,9 @@ stringy = " THis is '' a sample' text, get me?? Cool, bro;"
 
 
 def listmaker(stringthings):
-	newpuncs = [i for i in string.punctuation]
+	newpuncs = [i for i in string.punctuation if i != "'"]
 	string_without_punc = "".join(c for c in stringthings if c not in newpuncs)
 
 	return string_without_punc.split(" ")
 
 print listmaker(stringy)
-
